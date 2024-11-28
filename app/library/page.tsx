f874daafcb7adbbc7436a5249f56695e861b0e81
Matej Bendik <matej.bendik@student.tuke.sk>
2024-11-28 19:41:22 +0100

    Test push to new origin repo

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -2,7 +2,7 @@ export default function Library() {
   return (
     <div>
       <div className="flex flex-1 flex-col gap-4 p-4">
-        Library
+        Your Library
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           <div className="aspect-video rounded-xl bg-muted/50 border" />
           <div className="aspect-video rounded-xl bg-muted/50 border" />
